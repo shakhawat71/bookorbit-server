2e2146668a199e1faa5a83e6ca6aa42d68f9db53
Shakhawat Hossin <shakhawatipsc2000@gmail.com>
2026-03-06 20:28:50 +0600

    made some changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 // ===============================
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://bookorbit-388cb.web.app", ],
     credentials: true,
   })
 );

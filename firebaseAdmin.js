0f27e2bd3cd0dd976661771edaf1e30f41941ceb
Shakhawat Hossin <shakhawatipsc2000@gmail.com>
2026-03-06 06:53:19 +0600

    change back

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,31 +1,8 @@
-// firebaseAdmin.js
 const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-function initAdmin() {
-  // prevent re-init in serverless
-  if (admin.apps.length) return admin;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-  const projectId = process.env.FIREBASE_PROJECT_ID;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-    : null;
-
-  if (!projectId || !clientEmail || !privateKey) {
-    throw new Error(
-      "Missing Firebase env vars. Set FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY"
-    );
-  }
-
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId,
-      clientEmail,
-      privateKey,
-    }),
-  });
-
-  return admin;
-}
-
-module.exports = initAdmin();
+module.exports = admin;
